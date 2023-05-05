741d8067e674605c1e56668a4123a9d52c5700dc
Alen Šiljak <dev@alensiljak.eu.org>
2023-05-05 20:00:52 +0200

    input arguments, test files

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,41 @@
  * Implements all the logic and provides an entry point to 3rd-party code.
  */
 
-pub fn run() {
+/// entry point?
+pub fn run(args: Vec<String>) -> Vec<String> {
+    // here we should accept parameters:
+    // - command / report
+    // - input data (files/string)
+    // - filters
+
+    // stick to Ledger-compatible arguments?
+
     todo!("entry point")
+}
+
+/// Entry point for a report?
+fn report() {
+    todo!()
+}
+
+/// Parse input and return the model structure.
+fn parse() {
+    todo!()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::run;
+
+    #[test]
+    fn test_minimal() {
+        // create a ledger command
+        let command = "b -f tests/minimal.ledger";
+        let args = shell_words::split(command).expect("arguments parsed");
+
+        let actual = run(args);
+
+        todo!("get output back")
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
     println!("Hello, world!");
+
+    todo!("invoke the library with the command-line parameters")
 }
