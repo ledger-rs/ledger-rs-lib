db6f32551d8d15ce42ee90c5a955e5882b06f113
Alen Šiljak <dev@alensiljak.eu.org>
2023-08-04 09:34:04 +0200

    the pointers work

--- src/post.rs
+++ src/post.rs
@@ -3,6 +3,7 @@
  */
 
 use crate::{
+    account::Account,
     amount::Amount,
     journal::{AccountIndex, XactIndex},
 };
@@ -10,7 +11,7 @@ use crate::{
 #[derive(Debug, PartialEq)]
 pub struct Post {
     /// Pointer to the Account.
-    // pub account: AccountIndex,
+    account: *const Account,
     pub account_index: AccountIndex,
     /// Pointer to the Xact.
     pub xact: XactIndex,
@@ -21,7 +22,6 @@ pub struct Post {
     // assigned_amount
     // checkin
     // checkout
-
     pub note: Option<String>,
 }
 
@@ -35,6 +35,7 @@ impl Post {
         note: Option<&str>,
     ) -> Self {
         Self {
+            account: std::ptr::null(),
             account_index,
             xact: xact_index,
             amount,
@@ -42,7 +43,7 @@ impl Post {
             note: match note {
                 Some(content) => Some(content.to_owned()),
                 None => None,
-            }
+            },
         }
     }
 
@@ -51,6 +52,39 @@ impl Post {
     }
 }
 
+impl Default for Post {
+    fn default() -> Self {
+        Self {
+            account: std::ptr::null(),
+            account_index: Default::default(),
+            xact: Default::default(),
+            amount: Default::default(),
+            cost: Default::default(),
+            note: Default::default(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::account::Account;
+
+    use super::Post;
+
+    #[test]
+    fn test_pointers() {
+        const ACCT_NAME: &str = "Some Account";
+        let mut post = Post::default();
+
+        assert_eq!(std::ptr::null(), post.account);
+
+        // Assign account.
+        let acct = Account::new(ACCT_NAME);
+        post.account = &acct as *const Account;
+
+        unsafe {
+            // println!("account is {:?}", *post.account);
+            assert_eq!(ACCT_NAME, (*post.account).name);
+        }
+    }
 }
